85594579dddc23a797932721c612e9421f90ef30
ShelterIA <Irina.Shelter@vseinstrumenti.ru>
2020-12-05 17:16:28 +0300

    Домашняя работа №4, правка.

--- module_4/part_1/conftest.py
+++ module_4/part_1/conftest.py
@@ -3,14 +3,14 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-def pytest_addoptions(parser):
-    parser.addoptions('--language', action='store', default='en-gb',
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='ru',
                       help='Choose GUI language for tests')
 
 
 @pytest.fixture(scope="function")
 def browser(request):
-    language = request.config.getoptions('language')
+    language = request.config.getoption("language")
 
     if language not in ["ru", 'en-GB', 'es', 'fr']:
         raise pytest.UsageError("The wrong language is selected on the page")
@@ -22,7 +22,7 @@ def browser(request):
 
     result = webdriver.Chrome(options=options)
     result.maximize_window()
-    result.implicitly_wait()
+    result.implicitly_wait(5)
     result.user_language = language
 
     yield result
